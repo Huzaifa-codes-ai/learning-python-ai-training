3aeba439b89839d470b0e8519ed60ceb69397f16
Huzaifa-codes-ai <huzaifawork043@gmail.com>
2026-07-11 20:33:16 +0300

    Create for_loop_practice.py

---
+++ 02_Structures/for_loop_practice.py
@@ -0,0 +1,8 @@
+skills = ["Python", "VS Code", "Kaggle", "Logic"]
+
+print("--- Iterating Through Our List ---")
+
+for skill in skills:
+    print("I am mastering: " + skill)
+
+print("\nConveyor belt empty! Loop terminated automatically.")
