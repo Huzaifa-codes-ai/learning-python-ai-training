0f8e44932e3571586e3c39fd56d4d855a3d5cac1
Huzaifa-codes-ai <huzaifawork043@gmail.com>
2026-07-11 20:04:06 +0300

    Add user profile list and access items

---
+++ 01_Basics/list_practice.py
@@ -0,0 +1,12 @@
+user_profile = ["Alex", 25, 34.50, True]
+
+print("--- Accessing List Items ---")
+
+first_item = user_profile[0]
+print("First item (Index 0): " + first_item)
+
+age_item = user_profile[1]
+print("Second item (Index 1): " + str(age_item))
+
+status_item = user_profile[-1]
+print("Last item (Index -1): " + str(status_item))
